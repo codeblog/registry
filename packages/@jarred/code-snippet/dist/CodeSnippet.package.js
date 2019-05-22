81e32f306b9836ff42bfa1419dc7a27f3c9fa26c
@jarred <@jarred>
2019-05-21 18:02:29 -0700

    Published @jarred/code-snippet

---
+++ packages/@jarred/code-snippet/dist/CodeSnippet.package.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var _registry = require("/Users/jarred/.config/yarn/global/node_modules/codeblog/dist/registry");
+
+// This file will eventually be compiled into a package.json by codeblog cli
+module.exports = {
+  name: "@jarred/code-snippet",
+  version: "1.0.0",
+  license: "MIT",
+  codeblog: {
+    // These will appear when you search for a component
+    title: "Code Snippet",
+    description: "Source code, formatted nicely.",
+    // This is optional but recommended. If the file doesn't exist, it will be ignored.
+    screenshot: {
+      "1x": "https://storage.googleapis.com/codeblog-public/CodeBlock.png",
+      "2x": "https://storage.googleapis.com/codeblog-public/CodeBlock@2x.png",
+      "3x": "https://storage.googleapis.com/codeblog-public/CodeBlock@3x.png"
+    },
+    // If you use CategoryType.text, the component will be used on existing text, e.g. Glitter or Highlight. This is an inline.
+    // if you use CategoryType.header, the component will be used on a new block, e.g. the Fancy Header or Quote.
+    category: _registry.CategoryType.block,
+    // Optional: This lets you visually edit the props of a component in the editor
+    editableProps: {},
+    multiLine: true,
+    isVoid: false,
+    // This lets you set default props if you want
+    defaultProps: {
+      showLineNumbers: true,
+      disableStyles: false,
+      truncateLength: 20
+    }
+  },
+  // To add a dependency from npm, run:
+  // codeblog add code-block dependency-name
+  //
+  // For example:
+  // codeblog add code-block lodash
+  dependencies: {
+    "react-syntax-highlighter": "10.2.1"
+  }
+};
