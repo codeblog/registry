383910e416cda0417853094d151edb3626790ed3
@jarred <@jarred>
2019-05-30 03:21:45 -0700

    Published @jarred/facebook

---
+++ packages/@jarred/facebook/dist/Facebook.js
@@ -0,0 +1,98 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('@emotion/core'), require('react')) :
+  typeof define === 'function' && define.amd ? define(['@emotion/core', 'react'], factory) :
+  (global = global || self, global['@jarred/facebook'] = factory(global['@emotion/core'], global.react));
+}(this, function (core, react) { 'use strict';
+
+  react = react && react.hasOwnProperty('default') ? react['default'] : react;
+
+  function _extends() {
+    _extends = Object.assign || function (target) {
+      for (var i = 1; i < arguments.length; i++) {
+        var source = arguments[i];
+
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
+          }
+        }
+      }
+
+      return target;
+    };
+
+    return _extends.apply(this, arguments);
+  }
+
+  function _objectWithoutPropertiesLoose(source, excluded) {
+    if (source == null) return {};
+    var target = {};
+    var sourceKeys = Object.keys(source);
+    var key, i;
+
+    for (i = 0; i < sourceKeys.length; i++) {
+      key = sourceKeys[i];
+      if (excluded.indexOf(key) >= 0) continue;
+      target[key] = source[key];
+    }
+
+    return target;
+  }
+
+  function _objectWithoutProperties(source, excluded) {
+    if (source == null) return {};
+
+    var target = _objectWithoutPropertiesLoose(source, excluded);
+
+    var key, i;
+
+    if (Object.getOwnPropertySymbols) {
+      var sourceSymbolKeys = Object.getOwnPropertySymbols(source);
+
+      for (i = 0; i < sourceSymbolKeys.length; i++) {
+        key = sourceSymbolKeys[i];
+        if (excluded.indexOf(key) >= 0) continue;
+        if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue;
+        target[key] = source[key];
+      }
+    }
+
+    return target;
+  }
+
+  // This example will render text in uppercase. SHOUTING ON THE INTERNET
+
+  var _ref = {
+    name: "axl8za",
+    styles: "svg{width:1em;height:1em;}"
+  };
+
+  var Facebook = (function (_ref2) {
+    var children = _ref2.children,
+        otherProps = _objectWithoutProperties(_ref2, ["children"]);
+
+    return (// Since this is an inline component, it should be a `span` (or at least, `display: inline;`)
+      core.jsx("span", _extends({}, otherProps, {
+        // Webthing uses Emotion (https://emotion.sh) for CSS.
+        // This makes it easy to have styles that apply per component instead of to the whole page
+        css: _ref
+      }), core.jsx("svg", {
+        viewBox: "0 0 266.893 266.895"
+      }, core.jsx("path", {
+        d: "M252.164 266.895c8.134 0 14.729-6.596 14.729-14.73V14.73c0-8.137-6.596-14.73-14.729-14.73H14.73C6.593 0 0 6.594 0 14.73v237.434c0 8.135 6.593 14.73 14.73 14.73h237.434z",
+        fill: "#485a96"
+      }), core.jsx("path", {
+        d: "M184.152 266.895V163.539h34.692l5.194-40.28h-39.887V97.542c0-11.662 3.238-19.609 19.962-19.609l21.329-.01V41.897c-3.689-.49-16.351-1.587-31.08-1.587-30.753 0-51.807 18.771-51.807 53.244v29.705h-34.781v40.28h34.781v103.355h41.597z",
+        fill: "#fff"
+      })), " ", children)
+    );
+  }); // If you want to...
+  // - Supply default props
+  // - 🔎 Change how your component appears in search
+  // - 🎨 Change the props you can edit from the editor (e.g. accept a URL or a color)
+  // Edit this file:
+  // 📦/Users/jarred/Code/codeblog/jarred-components-4/Facebook.package.js
+
+  return Facebook;
+
+}));
