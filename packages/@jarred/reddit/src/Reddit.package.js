1e96a8ca127a06da9f9ff5f9e2de3a8dd45b2334
@jarred <@jarred>
2019-05-30 03:22:12 -0700

    Published @jarred/reddit

---
+++ packages/@jarred/reddit/src/Reddit.package.js
@@ -0,0 +1,32 @@
+// This file will eventually be compiled into a package.json by webthing cli
+module.exports = {
+  name: "@jarred/reddit",
+  version: "1.0.0",
+  license: "MIT",
+  webthing: {
+    // These will appear when you search for a component
+    title: "Reddit",
+    description: "TODO: Description",
+    // This is optional but recommended. If the file doesn't exist, it will be ignored.
+    screenshot: "./reddit.png",
+
+    // If you use CategoryType.text, the component will be used on existing text, e.g. Glitter or Highlight. This is an inline.
+    // if you use CategoryType.header, the component will be used on a new block, e.g. the Fancy Header or Quote.
+    category: "text",
+
+    // Optional: This lets you visually edit the props of a component in the editor
+    editableProps: {},
+
+    // This lets you set default props if you want
+    defaultProps: {
+      color: "pink"
+    }
+  },
+
+  // To add a dependency from npm, run:
+  // webthing add reddit dependency-name
+  //
+  // For example:
+  // webthing add reddit lodash
+  dependencies: {}
+};
