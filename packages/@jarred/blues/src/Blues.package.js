01e0e6980e9f6ce8832878a731891580852b729d
@jarred <@jarred>
2019-05-30 01:36:48 -0700

    Published @jarred/blues

---
+++ packages/@jarred/blues/src/Blues.package.js
@@ -0,0 +1,30 @@
+// This file will eventually be compiled into a package.json by webthing cli
+module.exports = {
+  name: "@jarred/blues",
+  version: "1.0.0",
+  license: "MIT",
+  webthing: {
+    // These will appear when you search for a component
+    title: "Blues",
+    description: "TODO: Description",
+    // This is optional but recommended. If the file doesn't exist, it will be ignored.
+    screenshot: "./blues.png",
+
+    // If you use CategoryType.text, the component will be used on existing text, e.g. Glitter or Highlight. This is an inline.
+    // if you use CategoryType.header, the component will be used on a new block, e.g. the Fancy Header or Quote.
+    category: "header",
+
+    // Optional: This lets you visually edit the props of a component in the editor
+    editableProps: {},
+
+    // This lets you set default props if you want
+    defaultProps: {}
+  },
+
+  // To add a dependency from npm, run:
+  // webthing add blues dependency-name
+  //
+  // For example:
+  // webthing add blues lodash
+  dependencies: {}
+};
