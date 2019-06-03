51c1bbfeee287653fbd857927ce03045465590bd
@jarred <@jarred>
2019-06-02 19:45:46 -0700

    Published @jarred/chat-bubble

---
+++ packages/@jarred/chat-bubble/dist/ChatBubble.esm.js
@@ -0,0 +1,87 @@
+import { jsx, css } from '@emotion/core';
+import React from 'react';
+import { WebthingContext } from '@webthing/core';
+import tinycolor from 'tinycolor2';
+
+function _objectWithoutPropertiesLoose(source, excluded) {
+  if (source == null) return {};
+  var target = {};
+  var sourceKeys = Object.keys(source);
+  var key, i;
+
+  for (i = 0; i < sourceKeys.length; i++) {
+    key = sourceKeys[i];
+    if (excluded.indexOf(key) >= 0) continue;
+    target[key] = source[key];
+  }
+
+  return target;
+}
+
+function _objectWithoutProperties(source, excluded) {
+  if (source == null) return {};
+
+  var target = _objectWithoutPropertiesLoose(source, excluded);
+
+  var key, i;
+
+  if (Object.getOwnPropertySymbols) {
+    var sourceSymbolKeys = Object.getOwnPropertySymbols(source);
+
+    for (i = 0; i < sourceSymbolKeys.length; i++) {
+      key = sourceSymbolKeys[i];
+      if (excluded.indexOf(key) >= 0) continue;
+      if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue;
+      target[key] = source[key];
+    }
+  }
+
+  return target;
+}
+
+// This is the React component that is shown your pad.
+// Since this is a Block component, be sure to render children. If you don't, things will break.
+
+var _ref = {
+  name: "kjafn5",
+  styles: "display:flex;position:relative;"
+};
+
+var ChatBubble = (function (_ref2) {
+  var children = _ref2.children,
+      background = _ref2.background,
+      otherProps = _objectWithoutProperties(_ref2, ["children", "background"]);
+
+  var _React$useContext = React.useContext(WebthingContext),
+      post = _React$useContext.post;
+
+  return jsx("div", {
+    // Webthing uses Emotion (https://emotion.sh) for CSS.
+    // This makes it easy to have styles that apply per component instead of to the whole page
+    css: _ref
+  }, post.blog.photo_url && jsx("img", {
+    src: post.blog.photo_url,
+    width: 24,
+    height: 24,
+    key: post.blog.photo_url,
+    style: {
+      borderRadius: "50%",
+      marginTop: "auto",
+      marginRight: "var(--offset-small, 4px)",
+      zIndex: 2
+    },
+    contentEditable: false
+  }), jsx("p", {
+    css:
+    /*#__PURE__*/
+    css("margin-block-start:0em;margin-block-end:0em;word-wrap:break-word;line-height:24px;position:relative;padding:10px 20px;border-radius:16px;font-family:-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,Helvetica,Arial,sans-serif,\"Apple Color Emoji\",\"Segoe UI Emoji\",\"Segoe UI Symbol\";font-family:var(--system-font);font-size:0.9em;&:before,&:after{content:\"\";position:absolute;bottom:0px;height:20px;}@media (min-width:670px){max-width:400px;}color:", tinycolor(background).isDark() ? "var(--color-white, #eee)" : "var(--color-black, #111)", ";background:", background, ";align-self:flex-end;" + ("")),
+    className: "ChatBubble"
+  }, children));
+}); // If you want to...
+// - Supply default props
+// - 🔎 Change how your component appears in search
+// - 🎨 Change the props you can edit from the editor (e.g. accept a URL or a color)
+// Edit this file:
+// 📦/Users/jarred/Code/codeblog/jarred-components-word/ChatBubble.package.js
+
+export default ChatBubble;
