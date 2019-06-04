3d6275a9a5a16d66381ad91ceaa6e40a64d5fa53
@jarred <@jarred>
2019-06-04 12:31:15 -0700

    Published @jarred/video-heading

--- packages/@jarred/video-heading/dist/VideoHeading.esm.js
+++ packages/@jarred/video-heading/dist/VideoHeading.esm.js
@@ -1,26 +1,25 @@
 import { jsx, css } from '@emotion/core';
-import React from 'react';
+import 'react';
 import tinycolor from 'tinycolor2';
-import { WebthingContext } from '@webthing/core';
+import { Webthing } from '@webthing/core';
+import moment from 'moment';
+import { trim } from 'lodash';
 
-// Since this is a Block component, be sure to render children. If you don't, things will break.
-
-var VideoHeading = (function (_ref) {
+var VideoHeading = function VideoHeading(_ref) {
   var children = _ref.children,
       fill = _ref.fill,
-      url = _ref.url;
-
-  var _React$useContext = React.useContext(WebthingContext),
-      post = _React$useContext.post;
-
+      url = _ref.url,
+      showDate = _ref.showDate,
+      href = _ref.href,
+      date = _ref.date;
   return jsx("a", {
-    href: post.url,
+    href: href,
     target: "_blank" // Webthing uses Emotion (https://emotion.sh) for CSS.
     // This makes it easy to have styles that apply per component instead of to the whole page
     ,
     css:
     /*#__PURE__*/
-    css("position:relative;box-sizing:content-box;text-decoration:none;margin-left:calc((100vw - var(--blog-post-width)) / -2);margin-right:calc((100vw - var(--blog-post-width)) / -2);padding-left:calc((100vw - var(--blog-post-width)) / 2);padding-right:calc((100vw - var(--blog-post-width)) / 2);background-size:100vw auto;font-size:1.75em;background-color:transparent;flex-direction:column;border-top:1px solid ", fill, ";border-bottom:1px solid ", fill, ";width:100%;margin-block-start:var(--offset-medium);padding-top:var(--offset-normal);padding-bottom:var(--offset-normal);margin-block-end:var(--offset-medium);font-weight:500;font-family:var(--headings-font);overflow-x:hidden;flex-direction:column;max-width:100vw;min-height:150px;display:flex;align-items:center;justify-content:center;&::before{content:\"\";display:block;position:absolute;top:0;left:0;bottom:0;right:0;opacity:0.5;background:linear-gradient( rgba(18,16,16,1) 50%,rgba(10,8,8,0.75) 50% ),linear-gradient( 90deg,rgba(255,0,0,0.94),rgba(0,255,0,0.98),rgba(0,0,255,0.94) );z-index:1;background-size:100% 2px,3px 100%;pointer-events:none;}.Text{z-index:2;position:relative;width:100%;height:100%;display:flex;flex:1;align-items:center;font-size:1.8em;text-align:left;color:", fill, ";text-shadow:0 0 1px ", tinycolor(fill).isDark() ? "#fff" : "#000", ";}@media (max-width:670px){&{margin-left:calc(-1 * var(--offset-normal));margin-right:calc(-1 * var(--offset-normal));padding-left:0;padding-right:0;width:auto;max-width:auto;}.Text{padding-left:var(--offset-normal);padding-right:var(--offset-normal);}}video{position:absolute;z-index:-1;width:100vw;height:100%;object-fit:cover;top:0;bottom:0;left:0;right:0;pointer-events:none;}" + (""))
+    css("@font-face{font-family:\"ProFontWindows\";src:url(\"https://js.webthing.co/nonjs/fonts/ProFontWindows.eot\");src:url(\"https://js.webthing.co/nonjs/fonts/ProFontWindows.eot?#iefix\") format(\"embedded-opentype\"),url(\"https://js.webthing.co/nonjs/fonts/ProFontWindows.otf\") format(\"opentype\"),url(\"https://js.webthing.co/nonjs/fonts/ProFontWindows.svg\") format(\"svg\"),url(\"https://js.webthing.co/nonjs/fonts/ProFontWindows.ttf\") format(\"truetype\"),url(\"https://js.webthing.co/nonjs/fonts/ProFontWindows.woff\") format(\"woff\"),url(\"https://js.webthing.co/nonjs/fonts/ProFontWindows.woff2\") format(\"woff2\");font-weight:normal;font-style:normal;}position:relative;box-sizing:content-box;text-decoration:none;margin-left:calc((100vw - var(--blog-post-width)) / -2);margin-right:calc((100vw - var(--blog-post-width)) / -2);padding-left:calc((100vw - var(--blog-post-width)) / 2);padding-right:calc((100vw - var(--blog-post-width)) / 2);background-size:100vw auto;font-size:1.75em;background-color:transparent;flex-direction:column;border-top:1px solid ", fill, ";border-bottom:1px solid ", fill, ";width:100%;margin-block-start:var(--offset-medium);padding-top:calc(var(--offset-normal) + 40px);padding-bottom:calc(var(--offset-normal) + 40px);margin-block-end:var(--offset-medium);font-weight:500;font-family:var(--headings-font);overflow-x:hidden;flex-direction:column;max-width:100vw;min-height:150px;display:flex;align-items:center;justify-content:center;&::before{content:\"\";display:block;position:absolute;top:0;left:0;bottom:0;right:0;background:linear-gradient( rgba(18,16,16,1) 50%,rgba(10,8,8,0.75) 50% ),linear-gradient( 90deg,rgba(255,0,0,0.94),rgba(0,255,0,0.98),rgba(0,0,255,0.94) );z-index:1;background-size:100% 2px,3px 100%;pointer-events:none;background-blend-mode:lighten;opacity:0.1;}.Text{z-index:2;position:relative;width:100%;height:100%;display:flex;line-height:1;flex:1;align-items:center;font-size:1.8em;line-height:1.2;text-align:left;color:", fill, ";text-shadow:0 0 1px ", tinycolor(fill).isDark() ? "#fff" : "#000", ";}@media (max-width:670px){&{margin-left:calc(-1 * var(--offset-normal));margin-right:calc(-1 * var(--offset-normal));padding-left:0;padding-right:0;width:auto;max-width:auto;}.Text{padding-left:var(--offset-normal);padding-right:var(--offset-normal);}.HomeVideo{left:48px;}}video{position:absolute;z-index:-1;width:100vw;height:100%;object-fit:cover;top:0;bottom:0;left:0;right:0;pointer-events:none;}.HomeVideo{position:absolute;width:var(--blog-post-width);left:0;right:0;margin-left:auto;margin-right:auto;bottom:40px;white-space:nowrap;display:grid;grid-row-gap:5px;grid-template-rows:22px 22px;z-index:0;opacity:0.8;}.Row{color:white;font-size:24px;line-height:24px;font-family:\"ProFontWindows\";text-transform:uppercase;display:flex;}.Left{text-align:right;width:50px;margin-right:20px;}" + (""))
   }, jsx("video", {
     playsInline: true,
     width: "100%",
@@ -31,9 +30,45 @@ var VideoHeading = (function (_ref) {
   }, jsx("source", {
     src: url,
     type: "video/mp4"
-  })), jsx("div", {
+  })), showDate && jsx("div", {
+    className: "HomeVideo"
+  }, jsx("div", {
+    className: "Row"
+  }, jsx("div", {
+    className: "Left"
+  }, date.format("a")), jsx("div", {
+    className: "Right"
+  }, date.format("HH:mm"))), jsx("div", {
+    className: "Row"
+  }, jsx("div", {
+    className: "Left"
+  }, date.format("MMM")), jsx("div", {
+    className: "Right"
+  }, date.format("DD YYYY")))), jsx("div", {
     className: "Text"
   }, children));
+}; // This is the React component that is shown your pad.
+// Since this is a Block component, be sure to render children. If you don't, things will break.
+
+
+var VideoHeading$1 = (function (_ref2) {
+  var children = _ref2.children,
+      text = _ref2.text,
+      fill = _ref2.fill,
+      url = _ref2.url;
+  return jsx(Webthing, null, function (_ref3) {
+    var _ref3$post = _ref3.post,
+        href = _ref3$post.url,
+        _ref3$post$updatedAt = _ref3$post.updatedAt,
+        updatedAt = _ref3$post$updatedAt === void 0 ? new Date() : _ref3$post$updatedAt;
+    return jsx(VideoHeading, {
+      date: moment(updatedAt || new Date()).subtract(25, "years"),
+      url: url,
+      href: href,
+      showDate: !text || trim(text).length === 0,
+      fill: fill
+    }, children);
+  });
 }); // If you want to...
 // - Supply default props
 // - 🔎 Change how your component appears in search
@@ -41,4 +76,4 @@ var VideoHeading = (function (_ref) {
 // Edit this file:
 // 📦/Users/jarred/Code/codeblog/jarred-components-word/VideoHeading.package.js
 
-export default VideoHeading;
+export default VideoHeading$1;
