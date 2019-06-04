8e7fd593c763d1654d077faca54ab735da59796b
@jarred <@jarred>
2019-06-03 17:19:09 -0700

    Published @jarred/chat-bubble

--- packages/@jarred/chat-bubble/dist/ChatBubble.esm.js
+++ packages/@jarred/chat-bubble/dist/ChatBubble.esm.js
@@ -1,7 +1,9 @@
-import { jsx, css } from '@emotion/core';
+import _styled from '@emotion/styled-base';
+import { jsx } from '@emotion/core';
 import React from 'react';
 import { WebthingContext } from '@webthing/core';
 import tinycolor from 'tinycolor2';
+import classNames from 'classnames';
 
 function _objectWithoutPropertiesLoose(source, excluded) {
   if (source == null) return {};
@@ -39,44 +41,80 @@ function _objectWithoutProperties(source, excluded) {
   return target;
 }
 
-// This is the React component that is shown your pad.
+var ChatBubble = _styled("div", {
+  target: "e5i1odf0"
+})({
+  name: "1qu2hqo",
+  styles: "display:flex;position:relative;margin-block-end:2px;p{border-top-right-radius:24px;border-top-left-radius:8px;border-bottom-right-radius:24px;border-bottom-left-radius:8px;}&.ChatBubble img{visibility:hidden;}&.ChatBubble--last img{visibility:visible;}&.ChatBubble--first p{border-top-left-radius:24px;}&.ChatBubble--last p{border-bottom-left-radius:24px;}&.ChatBubble--last p{}&.ChatBubble--last{margin-block-end:var(--offset-normal);}"
+});
+
+var Paragraph = _styled("p", {
+  target: "e5i1odf1"
+})({
+  name: "106p0e4",
+  styles: "margin-block-start:0em;margin-block-end:0em;word-wrap:break-word;line-height:24px;position:relative;padding:10px 20px;font-family:-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,Helvetica,Arial,sans-serif,\"Apple Color Emoji\",\"Segoe UI Emoji\",\"Segoe UI Symbol\";font-size:0.9em;&:before,&:after{content:\"\";position:absolute;bottom:0px;height:20px;}@media (min-width:670px){max-width:400px;}@media (max-width:670px){max-width:300px;}@media (max-width:400px){max-width:auto;padding:10px;}align-self:flex-end;"
+});
+
+var Photo = _styled("img", {
+  target: "e5i1odf2"
+})({
+  name: "1du7lp0",
+  styles: "border-radius:50%;margin-top:auto;margin-right:var(--offset-small,4px);z-index:2;@media (min-width:670px){margin-left:-24px;}"
+});
+
+var ChatBubbleNode = React.memo(function (_ref) {
+  var isFirst = _ref.isFirst,
+      isLast = _ref.isLast,
+      face = _ref.face,
+      background = _ref.background,
+      children = _ref.children;
+  return jsx(ChatBubble, {
+    className: classNames("ChatBubble", {
+      "ChatBubble--first": isFirst,
+      "ChatBubble--last": isLast
+    }) // Webthing uses Emotion (https://emotion.sh) for CSS.
+    // This makes it easy to have styles that apply per component instead of to the whole page
+
+  }, face && jsx(Photo, {
+    src: face,
+    width: 24,
+    height: 24,
+    key: face,
+    contentEditable: false
+  }), jsx(Paragraph, {
+    style: {
+      color: tinycolor(background).isDark() ? "var(--color-white, #eee)" : "var(--color-black, #111)",
+      background: background
+    }
+  }, children));
+}); // This is the React component that is shown your pad.
 // Since this is a Block component, be sure to render children. If you don't, things will break.
 
-var _ref = {
-  name: "kjafn5",
-  styles: "display:flex;position:relative;"
-};
-
-var ChatBubble = (function (_ref2) {
+var ChatBubble$1 = (function (_ref2) {
   var children = _ref2.children,
       background = _ref2.background,
-      otherProps = _objectWithoutProperties(_ref2, ["children", "background"]);
+      isSelected = _ref2.isSelected,
+      isFocused = _ref2.isFocused,
+      _ref2$groupOffset = _ref2.groupOffset,
+      face = _ref2.face,
+      _ref2$isFirst = _ref2.isFirst,
+      isFirst = _ref2$isFirst === void 0 ? true : _ref2$isFirst,
+      _ref2$isLast = _ref2.isLast,
+      isLast = _ref2$isLast === void 0 ? true : _ref2$isLast,
+      isInEditor = _ref2.isInEditor,
+      otherProps = _objectWithoutProperties(_ref2, ["children", "background", "isSelected", "isFocused", "groupOffset", "face", "isFirst", "isLast", "isInEditor"]);
 
   var _React$useContext = React.useContext(WebthingContext),
       post = _React$useContext.post;
 
-  return jsx("div", {
-    // Webthing uses Emotion (https://emotion.sh) for CSS.
-    // This makes it easy to have styles that apply per component instead of to the whole page
-    css: _ref
-  }, post.blog.photo_url && jsx("img", {
-    src: post.blog.photo_url,
-    width: 24,
-    height: 24,
-    key: post.blog.photo_url,
-    style: {
-      borderRadius: "50%",
-      marginTop: "auto",
-      marginRight: "var(--offset-small, 4px)",
-      zIndex: 2
-    },
-    contentEditable: false
-  }), jsx("p", {
-    css:
-    /*#__PURE__*/
-    css("margin-block-start:0em;margin-block-end:0em;word-wrap:break-word;line-height:24px;position:relative;padding:10px 20px;border-radius:16px;font-family:-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,Helvetica,Arial,sans-serif,\"Apple Color Emoji\",\"Segoe UI Emoji\",\"Segoe UI Symbol\";font-size:0.9em;&:before,&:after{content:\"\";position:absolute;bottom:0px;height:20px;}@media (min-width:670px){max-width:400px;}@media (max-width:670px){max-width:300px;}@media (max-width:400px){max-width:auto;}color:", tinycolor(background).isDark() ? "var(--color-white, #eee)" : "var(--color-black, #111)", ";background:", background, ";align-self:flex-end;" + ("")),
-    className: "ChatBubble"
-  }, children));
+  var _face = face || post.blog.photo_url;
+
+  return jsx(ChatBubbleNode, {
+    isFirst: isFirst,
+    isLast: isLast,
+    face: _face,
+    background: background
+  }, children);
 }); // If you want to...
 // - Supply default props
 // - 🔎 Change how your component appears in search
@@ -84,4 +122,4 @@ var ChatBubble = (function (_ref2) {
 // Edit this file:
 // 📦/Users/jarred/Code/codeblog/jarred-components-word/ChatBubble.package.js
 
-export default ChatBubble;
+export default ChatBubble$1;
