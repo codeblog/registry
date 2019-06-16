0bb4d77e97db571f151c716808a0561b6cec2193
@tandavas <@tandavas>
2019-06-15 20:49:32 -0700

    Published @tandavas/italic

---
+++ packages/@tandavas/italic/dist/Italic.js
@@ -0,0 +1,90 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('@emotion/core'), require('react')) :
+  typeof define === 'function' && define.amd ? define(['@emotion/core', 'react'], factory) :
+  (global = global || self, global['@tandavas/italic'] = factory(global['@emotion/core'], global.react));
+}(this, function (core, react) { 'use strict';
+
+  react = react && react.hasOwnProperty('default') ? react['default'] : react;
+
+  function _extends() {
+    _extends = Object.assign || function (target) {
+      for (var i = 1; i < arguments.length; i++) {
+        var source = arguments[i];
+
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
+          }
+        }
+      }
+
+      return target;
+    };
+
+    return _extends.apply(this, arguments);
+  }
+
+  function _objectWithoutPropertiesLoose(source, excluded) {
+    if (source == null) return {};
+    var target = {};
+    var sourceKeys = Object.keys(source);
+    var key, i;
+
+    for (i = 0; i < sourceKeys.length; i++) {
+      key = sourceKeys[i];
+      if (excluded.indexOf(key) >= 0) continue;
+      target[key] = source[key];
+    }
+
+    return target;
+  }
+
+  function _objectWithoutProperties(source, excluded) {
+    if (source == null) return {};
+
+    var target = _objectWithoutPropertiesLoose(source, excluded);
+
+    var key, i;
+
+    if (Object.getOwnPropertySymbols) {
+      var sourceSymbolKeys = Object.getOwnPropertySymbols(source);
+
+      for (i = 0; i < sourceSymbolKeys.length; i++) {
+        key = sourceSymbolKeys[i];
+        if (excluded.indexOf(key) >= 0) continue;
+        if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue;
+        target[key] = source[key];
+      }
+    }
+
+    return target;
+  }
+
+  // This example will render text in uppercase. SHOUTING ON THE INTERNET
+
+  var _ref = {
+    name: "1msjh1x",
+    styles: "font-style:italic;"
+  };
+
+  var Italic = (function (_ref2) {
+    var children = _ref2.children,
+        otherProps = _objectWithoutProperties(_ref2, ["children"]);
+
+    return (// Since this is an inline component, it should be a `span` (or at least, `display: inline;`)
+      core.jsx("span", _extends({}, otherProps, {
+        // Webthing uses Emotion (https://emotion.sh) for CSS.
+        // This makes it easy to have styles that apply per component instead of to the whole page
+        css: _ref
+      }), children)
+    );
+  }); // If you want to...
+  // - Supply default props
+  // - 🔎 Change how your component appears in search
+  // - 🎨 Change the props you can edit from the editor (e.g. accept a URL or a color)
+  // Edit this file:
+  // 📦/Users/tandavas/Projects/webthing/Italic.package.js
+
+  return Italic;
+
+}));
